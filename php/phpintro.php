199a150ba60688b08a0ed5e0d2ad08ccc634e421
vignesheg <vignesheg10@gmail.com>
2022-07-18 08:08:08 +0530

    init

---
+++ php/phpintro.php
@@ -0,0 +1,26 @@
+<?php 
+$name="senthil";
+$name="raja";
+/*
+echo "<html>\n";
+echo "\t<head>\n";
+echo "\t</head>\n";
+echo "<body>\n";
+echo "\t<h1>heading</h1>\n";
+echo "</body>\n";
+echo "</html>\n";
+*/
+$name="viky";
+?>
+<html>
+    <head></head>
+    <body>
+      <?php
+      echo date("l jS \of F Y h:i:s A");
+      ?>  
+    <h1>php programming</h1></body>
+    <?php
+       echo date("l jS \of F Y h:i:s A");   
+    ?>
+    <h2>Hello <?php echo $name;?></h2>
+</html>
